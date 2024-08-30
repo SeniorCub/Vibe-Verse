765650d037d597016ceac24cd6b0982667eaec1b
SeniorCub <farinderif@gmail.com>
2024-08-30 13:52:02 +0100

    worked on responsiveness

--- register.js
+++ register.js
@@ -16,20 +16,43 @@ function generateQRCode() {
      }
 }
 
+function upload() {
+     let input = document.querySelector("#file");
+     input.click();
+}
+
+function previewImage() {
+     const input = document.querySelector('#file');
+     const file = input.files[0];
+     const reader = new FileReader();
+
+     reader.addEventListener('load', function() {
+          const uploadPic = document.querySelector('#profile1');
+          uploadPic.innerHTML = `<img id="upload-pic" src="${reader.result}" alt="">`
+     });
+
+     if (file) {
+          reader.readAsDataURL(file);
+     }
+}
+
 // Next on Form one
 const next1 = document.querySelector('.next1').addEventListener("click", btn_next1);
 // Next and Back on Form 2
 const next2 = document.querySelector('.next2').addEventListener("click", btn_next2);
 const back2 = document.querySelector('.back2').addEventListener("click", btn_back2);
 // Next and Back on Form 3
-const next3 = document.querySelector('.next3').addEventListener("click", btn_next3);
+const next3 = document.querySelector('.next3');
+next3.addEventListener("click", btn_next3);
 const back3 = document.querySelector('.back3').addEventListener("click", btn_back3);
 // Back and Next on Form 4
 const back4 = document.querySelector('.back4').addEventListener("click", btn_back4);
 const next4 = document.querySelector('.next4');
 next4.addEventListener("click", btn_next4);
 const download = document.querySelector('.download');
+const payNow = document.querySelector('.payNow');
 
+let amountt = document.querySelector('#amount');
 // Inputs on Form 1
 let level = document.querySelector('#level');
 let lastName = document.querySelector('#last_name');
@@ -38,42 +61,52 @@ let userName = document.querySelector('#user_name');
 let email = document.querySelector('#email');
 let emaill = document.querySelector('#email');
 let uname = document.querySelector('#uname');
+let uploadPic = document.querySelector('#upload-pic')
 const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
 
 function payWithPaystack() {
-     let handler = PaystackPop.setup({
-     key: 'pk_test_7c8afac2a29e69900f51137136528cd58d92ff5e',
-     email: emaill,
-     amount: amountt.value * 100,
-     currency: 'NGN',
-     ref: 'PSK-' + Math.floor((Math.random() * 1000000000) + 1),
-     metadata: {
-          custom_fields: [
-               {
-                    display_name: "Manual",
-                    variable_name: "manual",
-                    value: 'Tuition Fees'
-               },
-               {
-                    display_name: "Amount",
-                    variable_name: "amount",
-                    value: amountt.value
-               },
-               {
-                    display_name: "Payment Type",
-                    variable_name: "payment_type",
-                    value: full.checked ? 'Full Payment' : 'Part Payment'
-               }
-          ]
-     },
-     callback: function(response) {
-          handlePaymentResponse(response);
-     },
-     onClose: function() {
-          alert('Payment window closed.');
+     if (email.value == "") {
+          alert('Error')
+     } else if (!emailPattern.test(email.value) ) {
+          alert('Invalid email uname');
+     }else{
+          let handler = PaystackPop.setup({
+          key: 'pk_test_7c8afac2a29e69900f51137136528cd58d92ff5e',
+          email: emaill.value,
+          amount: amountt.value * 100,
+          currency: 'NGN',
+          ref: 'PSK-' + Math.floor((Math.random() * 1000000000) + 1),
+          metadata: {
+               custom_fields: [
+                    {
+                         display_name: "Manual",
+                         variable_name: "manual",
+                         value: 'Tuition Fees'
+                    }
+                    // },
+                    // {
+                    //      display_name: "Amount",
+                    //      variable_name: "amount",
+                    //      value: amountt.value
+                    // },
+                    // {
+                    //      display_name: "Payment Type",
+                    //      variable_name: "payment_type",
+                    //      value: full.checked ? 'Full Payment' : 'Part Payment'
+                    // }
+               ]
+          },
+          callback: function(response) {
+               handlePaymentResponse(response);
+          },
+          onClose: function() {
+               alert('Payment window closed.');
+          }
+          });
+          handler.openIframe();
+          next3.style.display = 'block';
+          payNow.style.display = 'none';
      }
-     });
-     handler.openIframe();
 }
 
 // Step 1
@@ -98,14 +131,13 @@ function btn_next2() {
      const form3 = document.querySelector('.form3')
      const line3 =document.querySelector('.line3')
 
-     if (email.value == "" || uname.value == "") {
+     if (uname.value == "") {
           alert('Error')
-     } else if (!emailPattern.test(email.value) ) {
-          alert('Invalid email uname');
      } else {
           line3.style.display = 'block';
           form2.style.left = '-120vw';
           form3.style.left = '20vw';
+          next3.style.display = 'none';
      }
 };
 
@@ -114,6 +146,7 @@ function btn_back2() {
      const form2 = document.querySelector('.form2')
      const line2 =document.querySelector('.line2')
 
+    
      line2.style.display = 'none'
      form1.style.left = '20vw'
      form2.style.left = '120vw'
@@ -134,7 +167,7 @@ function btn_next3() {
      const form3 = document.querySelector('.form3')
      const form4 = document.querySelector('.form4')
      const line4 =document.querySelector('.line4')
-     
+    
      line4.style.display = 'block' 
      form3.style.left = '-120vw'
      form4.style.left = '20vw'
