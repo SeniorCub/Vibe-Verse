1909e9446ab2f23f68cd1d5fbf0e6138afb04eb1
seniorcub <farinderif@gmail.com>
2024-10-19 11:47:12 +0100

    Refactor Access-Control-Allow-Origin header in organizer/header.php

--- src/organizer/header.php
+++ src/organizer/header.php
@@ -3,7 +3,7 @@
 // Allow from any origin
 header("Access-Control-Allow-Origin: *");
 // Allow from specific origin
-header("Access-Control-Allow-Origin: http://127.0.0.1:5501");
+// header("Access-Control-Allow-Origin: http://127.0.0.1:5501");
 header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Credentials: true");
